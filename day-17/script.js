0299d555ad5edfafb69c5109196562db9f979915
Santosh910 <138295487+Santosh910@users.noreply.github.com>
2023-08-11 23:49:42 +0530

    update

---
+++ day-17/script.js
@@ -0,0 +1,38 @@
+// data Types
+
+// 1.string:
+
+// syntax:"",'',``
+// example:-
+// "santosh",'survesantosh09',`44`
+
+// 2.Number:
+
+// syntax:
+// example:55,454545,4545
+
+// 3.boolean:
+
+// syntax: true,false
+
+// 4.undefined
+
+// initially no value
+
+// variable:
+
+// 1.var
+// 2.let
+// 3.const
+
+var myname = "santosh";
+var mysurname = "surve";
+var myage = 28;
+var idtat;
+var mycourse = true;
+
+console.log(myname,typeof(myname));
+console.log(mysurname,typeof(mysurname));
+console.log(myage,typeof(myage));
+console.log(idtat,typeof(idtat));
+console.log(mycourse,(mycourse));
